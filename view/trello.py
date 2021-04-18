beea5e67f1c961078ba63550f5ce932956edd0ac
Toomore Chiang <toomore0929@gmail.com>
2021-04-18 07:39:32 +0000

    Add color and using card style

--- view/trello.py
+++ view/trello.py
@@ -26,6 +26,7 @@ def index():
     if 'action' in data:
         if 'type' in data['action'] and data['action']['type'] == 'commentCard':
             _action = data['action']
+            color = '#ff9f1a'
             wording = '**%s** 在 **[%s](%s)** 留言說明：\n%s' % (
                     _action['display']['entities']['memberCreator']['text'],
                     _action['data']['card']['name'],
@@ -35,6 +36,7 @@ def index():
 
         if 'type' in data['action'] and data['action']['type'] == 'createCard':
             _action = data['action']
+            color = '#0079bf'
             wording = '**%s** 新增一張卡片 **[%s](%s)** (%s)' % (
                     _action['display']['entities']['memberCreator']['text'],
                     _action['data']['card']['name'],
@@ -45,6 +47,7 @@ def index():
         if 'type' in data['action'] and data['action']['type'] == 'updateCard':
             _action = data['action']
             if 'old' in _action['data'] and 'name' in _action['data']['old']:
+                color = '#eb5a46'
                 wording = '**%s** 變更卡片名稱 **_%s_** -> **_[%s](%s)_**' % (
                         _action['display']['entities']['memberCreator']['text'],
                         _action['data']['old']['name'],
@@ -53,6 +56,7 @@ def index():
                 )
 
             if 'old' in _action['data'] and 'desc' in _action['data']['old']:
+                color = '#ff78cb'
                 wording = '**%s** 調整了 **[%s](%s)** 卡片內容。' % (
                         _action['display']['entities']['memberCreator']['text'],
                         _action['data']['card']['name'],
@@ -60,6 +64,7 @@ def index():
                 )
 
             if 'old' in _action['data'] and 'idList' in _action['data']['old']:
+                color = '#70b500'
                 wording = '**%s** 移動 **[%s](%s)** 卡片 **_%s_** -> **_%s_**' % (
                         _action['display']['entities']['memberCreator']['text'],
                         _action['data']['card']['name'],
@@ -71,9 +76,21 @@ def index():
     if wording:
         logging.info('trello wording: %s', wording)
 
+        title = _action['data']['card']['name']
+        title_link = 'https://trello.com/c/%s/%s' % (_action['data']['card']['shortLink'], _action['data']['card']['idShort'])
+
         requests.post(
                 setting.TRELLO_HOOK,
-                json={'text': wording, 'props': {'card': '```\n%s\n```' % _action}})
+                json={
+                        'icon_emoji': ':space_invader:',
+                        'attachments': [{
+                                'text': wording, 'color': color, 'title': title, 'title_link': title_link,
+                                'author_icon': 'https://trello.com/favicon.ico',
+                                'author_name': 'Trello',
+                                'author_link': title_link,
+                            }],
+                        'props': {'card': '```\n%s\n```' % _action, }
+                    })
 
     return ''
 
