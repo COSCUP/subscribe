bc629f9bd696a688f9c73c15632b2b56895d3402
Toomore Chiang <toomore0929@gmail.com>
2021-10-03 06:02:24 +0000

    PEP8

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-import logging
-logging.basicConfig(
-    filename='./log/log.log',
-    format='%(asctime)s [%(levelname)-5.5s][%(thread)6.6s] [%(module)s:%(funcName)s#%(lineno)d]: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    level=logging.DEBUG)
-
+''' main '''
 import hashlib
+import logging
 import os
 import traceback
-#import re
-from urllib.parse import parse_qs
-from urllib.parse import urlparse
+from urllib.parse import parse_qs, urlparse
 
 import arrow
 import google_auth_oauthlib.flow
 from apiclient import discovery
-from flask import Flask
-from flask import g
-from flask import got_request_exception
-from flask import redirect
-from flask import render_template
-from flask import request
-from flask import session
-from flask import url_for
+from flask import (Flask, g, got_request_exception, redirect, render_template,
+                   request, session, url_for)
 
 import setting
 from celery_task.task_mail_sys import mail_sys_weberror
@@ -31,6 +18,14 @@ from view.reader import VIEW_READER
 from view.subscribe import VIEW_SUBSCRIBE
 from view.subscriber import VIEW_SUBSCRIBER
 from view.trello import VIEW_TRELLO
+
+logging.basicConfig(
+    filename='./log/log.log',
+    format='%(asctime)s [%(levelname)-5.5s][%(thread)6.6s] [%(module)s:%(funcName)s#%(lineno)d]: %(message)s',  # pylint: disable=line-too-long
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG)
+
+#import re
 
 
 app = Flask(__name__)
@@ -50,14 +45,15 @@ NO_NEED_LOGIN_PATH = (
     '/exception',
 )
 
+
 @app.before_request
 def need_login():
-    app.logger.info('[X-SSL-SESSION-ID: %s] [X-REAL-IP: %s] [USER-AGENT: %s] [SESSION: %s]' % (
-            request.headers.get('X-SSL-SESSION-ID'),
-            request.headers.get('X-REAL-IP'),
-            request.headers.get('USER-AGENT'),
-            session, )
-       )
+    ''' need_login '''
+    logging.info('[X-SSL-SESSION-ID: %s] [X-REAL-IP: %s] [USER-AGENT: %s] [SESSION: %s]',
+                 request.headers.get('X-SSL-SESSION-ID'),
+                 request.headers.get('X-REAL-IP'),
+                 request.headers.get('USER-AGENT'),
+                 session, )
 
     if request.path not in NO_NEED_LOGIN_PATH \
             and not request.path.startswith('/subscriber') \
@@ -68,6 +64,8 @@ def need_login():
             session['r'] = request.path
             return redirect(url_for('oauth2logout', _scheme='https', _external=True))
 
+    return None
+
 
 @app.after_request
 def no_store(response):
@@ -77,21 +75,24 @@ def no_store(response):
 
     return response
 
+
 @app.route('/')
 def index():
+    ''' index '''
     return render_template('./index.html')
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
+    ''' oauth2callback '''
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         setting.CLIENT_SECRET,
         scopes=(
-          'openid',
-          'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile',
+            'openid',
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/userinfo.profile',
         ),
-        redirect_uri='https://%s/oauth2callback' % setting.DOMAIN,
+        redirect_uri=f'https://{setting.DOMAIN}/oauth2callback',
     )
 
     if 'code' not in request.args:
@@ -107,27 +108,28 @@ def oauth2callback():
     url = request.url.replace('http://', 'https://')
     url_query = parse_qs(urlparse(url).query)
 
-    if 'state' in url_query and url_query['state'] and url_query['state'][0] == session.get('state'):
+    if 'state' in url_query and url_query['state'] and \
+            url_query['state'][0] == session.get('state'):
         flow.fetch_token(authorization_response=url)
 
-        auth_client = discovery.build('oauth2', 'v2', credentials=flow.credentials, cache_discovery=False)
+        auth_client = discovery.build(
+            'oauth2', 'v2', credentials=flow.credentials, cache_discovery=False)
         user_info = auth_client.userinfo().get().execute()
 
         session['email'] = user_info['email'].lower()
         session['u'] = user_info
 
         if 'r' in session:
-            r = session['r']
-            app.logger.info('login r: %s' % r)
+            redirect_path = session['r']
+            logging.info('login r: %s', redirect_path)
             session.pop('r', None)
             session.pop('state', None)
-            return redirect(r)
+            return redirect(redirect_path)
 
         return redirect(url_for('index', _scheme='https', _external=True))
 
-    else:
-        session.pop('state', None)
-        return redirect(url_for('oauth2callback', _scheme='https', _external=True))
+    session.pop('state', None)
+    return redirect(url_for('oauth2callback', _scheme='https', _external=True))
 
 
 @app.route('/logout')
@@ -145,17 +147,23 @@ def oauth2logout():
 
     return redirect(url_for('index', _scheme='https', _external=True))
 
+
 @app.route('/exception')
-def exception():
+def exception_func():
+    ''' exception_func '''
     try:
         1/0
-    except Exception as e:
-        raise Exception('Error: [%s]' % e)
+    except ZeroDivisionError as error:
+        raise ZeroDivisionError('Error: [%s]', error)
+
 
 def error_exception(sender, exception, **extra):
+    ''' error_exception '''
+    logging.debug('sender: %s, exception: %s, extra: %s',
+                  sender, exception, extra)
     mail_sys_weberror.apply_async(
         kwargs={
-            'title': u'%s %s %s' % (request.method, request.path, arrow.now()),
+            'title': '%s %s %s' % (request.method, request.path, arrow.now()),
             'body': '''<b>%s</b> %s<br>
             <pre>%s</pre>
             <pre>%s</pre>
@@ -164,8 +172,8 @@ def error_exception(sender, exception, **extra):
              g.get('user', {}).get('account', {}).get('_id'), session.get('sid'), request.args, request.form, request.values, traceback.format_exc())
         })
 
+
 got_request_exception.connect(error_exception, app)
 
 if __name__ == '__main__':
     app.run(debug=False, host=setting.SERVER_HOST, port=setting.SERVER_PORT)
-
