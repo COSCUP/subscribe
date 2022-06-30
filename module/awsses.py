3331ed43dc2988143780d56a9c352bedb9e5676f
Toomore Chiang <toomore0929@gmail.com>
2022-06-30 06:47:19 +0000

    Update the ses body
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- module/awsses.py
+++ module/awsses.py
@@ -116,7 +116,7 @@ class AWSSES(object):
         :rtype: :py:class:`email.mime.multipart.MIMEMultipart`
 
         '''
-        msg_all = MIMEMultipart()
+        msg_all = MIMEMultipart('mixed')
         msg_all['From'] = self.format_mail(
             self.source['name'], self.source['mail'])
 
@@ -138,7 +138,13 @@ class AWSSES(object):
         if 'x_coscup' in kwargs and kwargs['x_coscup']:
             msg_all['X-Coscup'] = kwargs['x_coscup']
 
-        msg_all.attach(MIMEText(kwargs['body'], 'html', 'utf-8'))
+        if 'list_unsubscribe' in kwargs and kwargs['list_unsubscribe']:
+            msg_all['List-Unsubscribe'] = kwargs['list_unsubscribe']
+
+        body_mine = MIMEMultipart('alternative')
+        body_mine.attach(MIMEText(kwargs['body'], 'html', 'utf-8'))
+
+        msg_all.attach(body_mine)
 
         if 'attachment' in kwargs and kwargs['attachment']:
             for attach in kwargs['attachment']:
